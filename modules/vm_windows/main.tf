12f88e3cdf237b78f9f76287ae42dcbf2f9dd3ac
kachalio <>
2026-08-21 10:18:14 -0400

    adds windows vm module

--- modules/vm_windows/main.tf
+++ modules/vm_windows/main.tf
@@ -1,4 +1,42 @@
-data "azurerm_resource_group" "rg" {
-  name     = "example-resource-group"
-  location = var.rg_location
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "${var.vm_name}-ipconfig"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                 = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.windows_vm_size
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id,
+  ]
+  admin_username                  = var.vm_admin_username
+  admin_password                  = var.vm_admin_password
+  # disable_password_authentication = "false"
+  
+
+  source_image_reference {
+    publisher = var.windows_vm_image.publisher
+    offer     = var.windows_vm_image.offer
+    sku       = var.windows_vm_image.sku
+    version   = var.windows_vm_image.version
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.vm_os_disk_storage_account_type
+  }
+
+  tags = var.tags
 }
--- modules/vm_windows/variables.tf
+++ modules/vm_windows/variables.tf
@@ -1,4 +1,66 @@
-variable "rg_location" {
+variable "location" {
   description = "The location of the resource group"
   type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "The name of the Windows VM"
+  type        = string
+  default     = "tf-asr-windows-vm"
+}
+
+variable "windows_vm_size" {
+  description = "The size of the Windows VM"
+  type        = string
+  default     = "Standard_B2s"
+}
+
+variable "vm_admin_username" {
+  description = "The admin username for the Windows VM"
+  type        = string
+}
+
+variable "vm_admin_password" {
+  description = "The admin password for the Windows VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "windows_vm_image" {
+  description = "The image reference for the Windows VM"
+  type = object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  })
+  default = {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+variable "vm_os_disk_storage_account_type" {
+  description = "The storage account type for the OS disk"
+  type        = string
+  default     = "StandardSSD_LRS"
+}
+
+
+variable "tags" {
+  description = "A map of tags to assign to the resources"
+  type        = map(string)
+  default     = {}
 }
